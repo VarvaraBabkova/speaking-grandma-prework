ef1768a2e99d6e2fd60f74ff7eee9daad4899de6
Varvara Babkova <varvara.munday@gmail.com>
2019-06-06 13:46:07 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,9 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma (str)
   upstr = str.upcase
+  if str == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  end
   if str != upstr
     return "HUH?! SPEAK UP, SONNY!"
   else
